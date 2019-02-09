cea7ecb8805f5c633a2fb90c6b7026adec9c3401
Николай Лабазов <Nickolaylabazov@yandex.ru>
2019-02-09 13:19:58 +0300

    correction

--- function.js
+++ function.js
@@ -2,10 +2,10 @@ export function hero(obj) {
 	if (obj.health > 50) {
 		return `healthy`;
 	}
-	if ((obj.health > 15) & (obj.health <= 50)) {
+	else if (obj.health > 15) {
 		return `wounded`;
 	}
-	if (obj.health <= 15) {
+	else {
 		return `critical`;
-    } 
+  } 
 };
